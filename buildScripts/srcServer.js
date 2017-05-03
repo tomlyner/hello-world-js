02d7ef991cfd447b80d99fd6a8a7ec498a6f7808
tomlyner <lyner@mistral.co.uk>
2017-05-03 22:12:51 +0100

    Add webpack and index js with simple log

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var path = require('path');
 var open = require('open');
+var webpack = require('webpack');
+var config = require('../webpack.config.dev');
 
-var port = 3000;
+const port = 3000;
+const app = express();
+const compiler = webpack(config);
 
-var app = express();
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
---
+++ webpack.config.dev.js
@@ -0,0 +1,21 @@
+var path = require('path');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
+      {test: /\.css$/, loaders: ['style','css']}
+    ]
+  }
+};
