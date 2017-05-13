0ee87d4b9b448918d107ac711c11a78f7787aea0
tomlyner <lyner@mistral.co.uk>
2017-05-13 19:54:12 +0100

    Put vendor js in separate bundle

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,19 +1,25 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 var BabiliPlugin = require('babili-webpack-plugin');
 
 module.exports = {
   devtool: 'source-map',
-  entry: [
-    path.resolve(__dirname, 'src/index')
-  ],
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor' // Specify the common bundle's name.
+    }),
+
     new HtmlWebpackPlugin({
       template: 'src/index.html',
       inject: true
